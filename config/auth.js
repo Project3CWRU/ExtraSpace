245a1ec15fb89f87f656806919abb5cb875e6114
Janesz <JaneszS@ccf.org>
2018-01-13 00:40:32 -0500

    heroku again

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'google' : {
         'clientID'      : '764364046531-j2it2uercojjq3j819nh38jj5vg175s7.apps.googleusercontent.com',
         'clientSecret'  : 'Q1NNuz-e7pttHihkoFyRuF_C',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'http://gentle-harbor-59380.herokuapp.com/auth/google/callback'
     }
 
 };
